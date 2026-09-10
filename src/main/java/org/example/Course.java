a1523a560b9d009dc79ef3746fdfc4effb7cfbf2
Mishca Khan <khanma6@farmingdale.edu>
2026-09-09 22:44:53 -0400

    Completed Assignment 1

--- src/main/java/org/example/Course.java
+++ src/main/java/org/example/Course.java
@@ -1,29 +1,43 @@
 package org.example;
 
-/*
-* ToDo 01:
- Complete the followings:
+    //Has the information for a course
+public class Course {
 
-The Course class should have:
-
-    - ID
-
-    - Name
-
-    - Code
-
- - You need to have setters and getters for all data members
- - You must have a default constructor and an overloaded one that accepts 3 parameters to update the member variables.
-
- - Using a driver class (with main), instantiate the Course class and call its methods to change the id, name, and code.
-
- - Test and run your code.
-
- - Add comments to explain your program.
-
- - Push the code to GitHub.
-
- - Submit the Github link here (make sure it is a public repo).
-
- */
-
+        //These are the variables
+    private int id;
+    private String name;
+    private String code;
+        //this is the default constructor
+    public Course() {
+        id = 0;
+        name = "";
+        code = "";
+    }
+        //This is the constructor that lets you enter the information
+    public Course(int id, String name, String code) {
+        this.id = id;
+        this.name = name;
+        this.code = code;
+    }
+        //These lines get and set the course ID
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+        //These lines get and set the course name
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+        //These lines get and set the course code
+    public String getCode() {
+        return code;
+    }
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
--- src/main/java/org/example/DriverClass.java
+++ src/main/java/org/example/DriverClass.java
@@ -1,10 +1,26 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class DriverClass {
     public static void main(String[] args) {
+            //This creates a course while using the default constructor
+        Course course1 = new Course();
+            //This changes the information in the first course
+        course1.setId(101);
+        course1.setName("Data Structures & Algorithms");
+        course1.setCode("CSC229");
+            //This prints out the information for the first course
+        System.out.println("Course 1");
+        System.out.println("ID: " + course1.getId());
+        System.out.println("Name: " + course1.getName());
+        System.out.println("Code: " + course1.getCode());
 
+        System.out.println();
+            //This makes the second course from the overloaded constructor
+        Course course2 = new Course(102, "Management Information Systems", "BCS300");
+            //This prints out the information for the second course
+        System.out.println("Course 2");
+        System.out.println("ID: " + course2.getId());
+        System.out.println("Name: " + course2.getName());
+        System.out.println("Code: " + course2.getCode());
     }
-
 }
